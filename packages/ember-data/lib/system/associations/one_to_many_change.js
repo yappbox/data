b2a792b1a1983b9d172c53993b460b538d47664a
Stefan Penner <stefan.penner@gmail.com>
2013-02-22 16:02:05 -0500

    fix deletiong belongs_to/belongs_to

--- packages/ember-data/lib/system/associations/one_to_many_change.js
+++ packages/ember-data/lib/system/associations/one_to_many_change.js
@@ -249,14 +249,14 @@ DS.OneToManyChange.prototype = {
       // If our lastParent clientId is not null, there will always be a
       // materialized lastParent.
       var lastParent = this.getLastParent();
-      if (lastParent) {
+      if (lastParent && hasManyName) {
         lastParent.suspendAssociationObservers(function() {
           get(lastParent, hasManyName).removeObject(child);
         });
       }
 
       // Don't do anything if the belongsTo is going from null back to null
-      if (oldParent) {
+      if (oldParent && hasManyName) {
         get(oldParent, hasManyName).addObject(child);
       }
 
@@ -288,7 +288,7 @@ DS.OneToManyChange.prototype = {
     // 1. The change happened from the belongsTo side
     // 2. The record was moved to a new parent without explicitly
     //    removing it from the old parent first.
-    if (oldParent && oldParent !== newParent) {
+    if (oldParent && oldParent !== newParent && hasManyName) {
       get(oldParent, hasManyName).removeObject(child);
 
       // TODO: This implementation causes a race condition in key-value
@@ -305,7 +305,7 @@ DS.OneToManyChange.prototype = {
     // to ensure that the record is in its ManyArray. The `addObject`
     // method only has an effect if the change happened from the
     // belongsTo side.
-    if (newParent) {
+    if (newParent && hasManyName) {
       get(newParent, hasManyName).addObject(child);
 
       if (get(newParent, 'isLoaded')) {
